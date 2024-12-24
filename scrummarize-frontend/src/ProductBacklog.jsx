0f4766b8806f0b69cef86b59e1c6bb8173a3ec09
Ren <ren12death@gmail.com>
2024-12-24 18:27:11 +0800

    feat(task card): add Task Card component
    - Create TaskCard.jsx
    - Create TaskCard.css
    - Add Task Card component to ProductBacklog.jsx

--- scrummarize-frontend/src/ProductBacklog.jsx
+++ scrummarize-frontend/src/ProductBacklog.jsx
@@ -1,15 +1,37 @@
+import { useState } from 'react'
 import './ProductBacklog.css'
+import TaskCard from './TaskCard.jsx'
 
 function ProductBacklog() {
-  return (
-    <section className="product-backlog">
-        <h2>Product Backlog</h2>
 
-        <div className="product-backlog__task-list">
+    const [tasks, setTasks] = useState([
+        {   
+            id: 1,
+            taskName: "Draw a cat",
+            storyPoint: 100,
+            priorityRating: "Important",
+            tags: ["Frontend", "Backend", "API", "Database", "Framework", "Testing", "UI", "UX"]
+        }, 
+        {   
+            id: 2,
+            taskName: "Read a book",
+            storyPoint: 1,
+            priorityRating: "Low",
+            tags: ["Frontend"]
+        }
+    ])
 
-        </div>
-    </section>
-  )
+    return (
+        <section className="product-backlog">
+            <h2>Product Backlog</h2>
+
+            <div className="product-backlog__task-list">
+                {
+                    tasks.map(task => <TaskCard task={task} key={task.id} />)
+                }
+            </div>
+        </section>
+    )
 }
 
 export default ProductBacklog
---
+++ scrummarize-frontend/src/TaskCard.jsx
@@ -0,0 +1,83 @@
+import './TaskCard.css'
+
+function TaskCard(props) {
+
+    const getPriorityRatingClass = priorityRating => {
+        const defaultClass = "task-card__priority-rating"
+
+        let priorityRatingClass = ""
+        switch(priorityRating) {
+            case "Low":
+                priorityRatingClass = defaultClass + "--low"
+                break
+            case "Medium":
+                priorityRatingClass = defaultClass + "--medium"
+                break
+            case "Important":
+                priorityRatingClass = defaultClass + "--important"
+                break
+            case "Urgent":
+                priorityRatingClass = defaultClass + "--urgent"
+                break
+            default:
+                priorityRatingClass = ""
+        }
+
+        return defaultClass + " " + priorityRatingClass
+    }
+
+    const getTagClass = tag => {
+        const defaultClass = "task-card__tag"
+
+        let tagClass = ""
+        switch(tag) {
+            case "Frontend":
+                tagClass = defaultClass + "--frontend"
+                break
+            case "Backend":
+                tagClass = defaultClass + "--backend"
+                break
+            case "API":
+                tagClass = defaultClass + "--api"
+                break
+            case "Database":
+                tagClass = defaultClass + "--database"
+                break
+            case "Framework":
+                tagClass = defaultClass + "--framework"
+                break
+            case "Testing":
+                tagClass = defaultClass + "--testing"
+                break
+            case "UI":
+                tagClass = defaultClass + "--ui"
+                break
+            case "UX":
+                tagClass = defaultClass + "--ux"
+                break
+            default:
+                tagClass = ""
+        }
+
+        return defaultClass + " " + tagClass
+    }
+
+    return (
+        <article className="task-card">
+            <h3>{props.task.taskName}</h3>
+            <div className="task-card__information">
+                <div className="task-card__line">
+                    <p className="task-card__story-point">{props.task.storyPoint}</p>
+                    <p className={getPriorityRatingClass(props.task.priorityRating)}>{props.task.priorityRating}</p>
+                </div>
+                <div className="task-card__tags">
+                    {
+                        props.task.tags.map(tag => <p className={getTagClass(tag)} key={tag}>{tag}</p>)
+                    }
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default TaskCard
