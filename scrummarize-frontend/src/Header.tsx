db5f3fd30cedd8e5527cc31c0d6d4a009dc6b870
Ren <ren12death@gmail.com>
2025-01-03 21:58:30 +0800

    feat(header): add nav bar
    - Add a nav bar to navigate between Product Backlog and Sprint Board

--- scrummarize-frontend/src/Header.tsx
+++ scrummarize-frontend/src/Header.tsx
@@ -1,12 +1,27 @@
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Link } from '@tanstack/react-router'
+import { useState } from 'react'
 
 function Header() {
+
+  const [showNav, setShowNav] = useState(false)
+
   return (
     <header className="header">
-        <h1>Scrummarize</h1>
-        <FontAwesomeIcon icon={faBars} className="header__user-icon"/>
+        <div className="header__title-bar">
+          <h1>Scrummarize</h1>
+          <FontAwesomeIcon 
+            icon={faBars}
+            className="header__bar-icon"
+            onClick={() => setShowNav(prevShowNav => !prevShowNav)}
+          />
+        </div>
+        <nav className={showNav ? "header__nav" : "header__nav--hide"}>
+          <Link to="/product-backlog" className="header__nav__link">Product Backlog</Link>
+          <Link to="/sprint-board" className="header__nav__link">Sprint Board</Link>
+        </nav>
     </header>
   )
 }
