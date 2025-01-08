bd2e0eed10dc407a2b8cba2601f7c6bc3ee8feb0
Ren <ren12death@gmail.com>
2025-01-09 02:23:04 +0800

    feat(product backlog): Implement drag and drop Small Task Card
    - Install dnd-kit/core
    - Implement drag and drop in task.move.tsx

--- scrummarize-frontend/src/MoveList.tsx
+++ scrummarize-frontend/src/MoveList.tsx
@@ -1,13 +1,19 @@
+import { useDraggable, useDroppable } from '@dnd-kit/core';
 import { Sprint, Task } from './lib/types';
 import './MoveList.css';
 
 type MoveListProps = {
+  id: string;
   title?: string;
   sprints?: Sprint[];
   tasks: Task[];
 };
 
 function MoveList(props: MoveListProps) {
+  const { setNodeRef } = useDroppable({
+    id: props.id,
+  });
+
   return (
     <section className="main__section main__section--gray">
       {props.title && <h2 className="move-list__title">{props.title}</h2>}
@@ -19,10 +25,13 @@ function MoveList(props: MoveListProps) {
         </select>
       )}
 
-      <div className="main__section__list">
+      <div className="main__section__list" ref={setNodeRef}>
         {props.tasks.map((task) => (
           <SmallTaskCard key={task.taskID} task={task} />
         ))}
+        <div className="card move-list__invisible">
+          <h3>Invisible</h3>
+        </div>
       </div>
     </section>
   );
@@ -33,8 +42,24 @@ type SmallTaskCardProps = {
 };
 
 function SmallTaskCard(props: SmallTaskCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: props.task.taskID,
+  });
+
+  const style = transform
+    ? {
+        transform: `translate(${transform.x}px, ${transform.y}px)`,
+      }
+    : undefined;
+
   return (
-    <div className="card blue-container">
+    <div
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+      className="card blue-container move-list__card"
+      style={style}
+    >
       <h3>{props.task.name}</h3>
     </div>
   );
--- scrummarize-frontend/src/routes/product-backlog/task.move.tsx
+++ scrummarize-frontend/src/routes/product-backlog/task.move.tsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import '../../ProductBacklogMove.css';
 import { Sprint, Task } from '../../lib/types';
+import { useState } from 'react';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
 
 export const Route = createFileRoute('/product-backlog/task/move')({
   component: ProductBacklogMove,
@@ -29,11 +31,48 @@ const fetchData = async () => {
 function ProductBacklogMove() {
   const { tasks, sprints } = Route.useLoaderData();
 
+  const [pbTasks, setPBTasks] = useState(tasks);
+  const [sbTasks, setSBTasks] = useState<Task[]>([]);
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (!over) return;
+
+    const taskID = active.id as string;
+    const newList = over.id as string;
+
+    if (newList == 'sprintBacklog') {
+      const task = pbTasks.find((task) => task.taskID == taskID);
+
+      if (task) {
+        setPBTasks((prevPBTasks) =>
+          prevPBTasks.filter((task) => task.taskID != taskID)
+        );
+        setSBTasks((prevSBTasks) => [...prevSBTasks, task]);
+      }
+    } else if (newList == 'productBacklog') {
+      const task = sbTasks.find((task) => task.taskID == taskID);
+
+      if (task) {
+        setSBTasks((prevSBTasks) =>
+          prevSBTasks.filter((task) => task.taskID != taskID)
+        );
+        setPBTasks((prevPBTasks) => [...prevPBTasks, task]);
+      }
+    }
+  }
+
   return (
     <section className="move-component">
-      <MoveList title={'Product Backlog'} tasks={tasks} />
-      <FontAwesomeIcon icon={faArrowDown} className="move-component__arrow" />
-      <MoveList sprints={sprints} tasks={[]} />
+      <DndContext onDragEnd={handleDragEnd}>
+        <MoveList
+          id={'productBacklog'}
+          title={'Product Backlog'}
+          tasks={pbTasks}
+        />
+        <FontAwesomeIcon icon={faArrowDown} className="move-component__arrow" />
+        <MoveList id={'sprintBacklog'} sprints={sprints} tasks={sbTasks} />
+      </DndContext>
     </section>
   );
 }
